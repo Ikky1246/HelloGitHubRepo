3ddf8728901b1df5eed902d4e8db742fa988d573
Dndrskyy <dindariski1246@gmail.com>
2025-03-04 19:25:10 +0700

    Update Polymorphism.java

--- Polymorphism.java
+++ Polymorphism.java
@@ -1,101 +1,83 @@
-// Kelas induk (superclass) abstrak
+// Kelas abstrak sebagai superclass
 abstract class BangunDatar {
     abstract double luas();
     abstract double keliling();
-
+    
     public void tampilkanInfo() {
         System.out.println("Luas: " + luas());
         System.out.println("Keliling: " + keliling());
-        System.out.println("-----------");
+        System.out.println("--------------------");
     }
 }
 
 // Kelas Persegi
 class Persegi extends BangunDatar {
     double sisi;
-
-    public Persegi(double sisi) {
+    
+    Persegi(double sisi) {
         this.sisi = sisi;
     }
-
+    
     @Override
     double luas() {
         return sisi * sisi;
     }
-
+    
     @Override
     double keliling() {
         return 4 * sisi;
-    }
-
-    @Override
-    public void tampilkanInfo() {
-        System.out.println("=== Persegi ===");
-        super.tampilkanInfo();
     }
 }
 
 // Kelas Lingkaran
 class Lingkaran extends BangunDatar {
     double r;
-
-    public Lingkaran(double r) {
+    
+    Lingkaran(double r) {
         this.r = r;
     }
-
+    
     @Override
     double luas() {
         return Math.PI * r * r;
     }
-
+    
     @Override
     double keliling() {
         return 2 * Math.PI * r;
-    }
-
-    @Override
-    public void tampilkanInfo() {
-        System.out.println("=== Lingkaran ===");
-        super.tampilkanInfo();
     }
 }
 
 // Kelas Segitiga
 class Segitiga extends BangunDatar {
     double alas, tinggi;
-
-    public Segitiga(double alas, double tinggi) {
+    
+    Segitiga(double alas, double tinggi) {
         this.alas = alas;
         this.tinggi = tinggi;
     }
-
+    
     @Override
     double luas() {
         return 0.5 * alas * tinggi;
     }
-
+    
     @Override
     double keliling() {
-        return 3 * alas;
-    }
-
-    @Override
-    public void tampilkanInfo() {
-        System.out.println("=== Segitiga ===");
-        super.tampilkanInfo();
+        return alas + tinggi + Math.sqrt(alas * alas + tinggi * tinggi);
     }
 }
 
 // Kelas Main
 public class Main {
     public static void main(String[] args) {
-        BangunDatar[] bangunDatar = {
+        BangunDatar[] bangunList = {
             new Persegi(4),
             new Lingkaran(7),
-            new Segitiga(6, 8)
+            new Segitiga(3, 4)
         };
-
-        for (BangunDatar bd : bangunDatar) {
+        
+        for (BangunDatar bd : bangunList) {
             bd.tampilkanInfo();
         }
     }
